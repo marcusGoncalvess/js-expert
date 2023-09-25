217b0730eac55c0e51845870ca096fef48cf4e78
Marcus Gonçalves <marcusgoncalves067@gmail.com>
2023-09-25 01:29:09 -0300

    javascript fundamentals: esmodules pt-2

--- modulo03/demo02-esmodules-internacionalization/src/index.js
+++ modulo03/demo02-esmodules-internacionalization/src/index.js
@@ -1,27 +1,36 @@
-import DraftLog from 'draftlog';
-import chalk from 'chalk';
-import chalkTable from 'chalk-table';
-import readline from 'readline';
 
-import database from '../database.json' assert { type: 'json' };
-import Person from './person.js';
 
-DraftLog(console).addLineListener(process.stdin);
-const DEFAULT_LANG = 'pt-BR';
+import database from './../database.json'
+import Person from './person.js'
+import TerminalController from './terminalController.js'
+import { save } from './repository.js'
+const DEFAULT_LANG = "pt-BR"
+// const DEFAULT_LANG = "es"
+// const DEFAULT_LANG = "en"
+// const DEFAULT_LANG = "rus"
+const STOP_TERM = ":q"
 
-const options = {
-  leftPad: 2,
-  columns: [
-    { field: 'id', name: chalk.cyan('ID') },
-    { field: 'vehicles', name: chalk.magenta('Vehicles') },
-    { field: 'kmTraveled', name: chalk.cyan('Km Traveled') },
-    { field: 'from', name: chalk.cyan('From') },
-    { field: 'to', name: chalk.cyan('To') },
-  ],
-};
+const terminalController = new TerminalController()
+terminalController.initializeTerminal(database, DEFAULT_LANG)
 
-const table = chalkTable(
-  options,
-  database.map((item) => new Person(item).formatted(DEFAULT_LANG))
-);
-const print = console.draft(table);
+async function mainLoop() {
+    try {
+        const answer = await terminalController.question()
+        if(answer === STOP_TERM) {
+            terminalController.closeTerminal()
+            console.log('process finished!')
+            return;
+        }
+        const person = Person.generateInstanceFromString(answer)
+        terminalController.updateTable(person.formatted(DEFAULT_LANG))
+        await save(person)
+
+        return mainLoop()
+
+    } catch (error) {
+        console.error('DEU RUIM**', error)
+        return mainLoop()
+    }
+}
+
+await mainLoop()
